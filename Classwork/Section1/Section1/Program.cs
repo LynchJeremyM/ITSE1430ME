8e2593f9046b9a23934199593054049b9a9cadbe
LynchJeremyM <jeremy.lynch@my.tccd.edu>
2018-09-12 18:35:40 -0500

    OBJECTS

--- Classwork/Section1/Section1/Program.cs
+++ Classwork/Section1/Section1/Program.cs
@@ -19,6 +19,58 @@ namespace Section1
             //PlayWithStrings();
         }
 
+        private static void PlayWithObjects()
+        {
+            int hours = 10;
+            Int32 hoursFull = 10;
+            var areEqual = hours == hoursFull;
+
+            var obj1 = "Hello";
+            DisplayObject(obj1);
+        }
+
+        private static void DisplayObject ( object value )
+        {
+            if (value == null)
+                return;
+
+            //Approach 1
+            if (value is string)
+            {
+                var str = (string)value;
+                Console.WriteLine(str);
+            } else
+            {
+                var str = value.ToString();
+                Console.WriteLine(str);
+            }
+
+            //Approach 2
+            var str2 = value as string;
+            if (str2 != null)
+                Console.WriteLine(value.ToString());
+            else
+                Console.WriteLine(value.ToString());
+
+            //Approach 3
+            var str3 = value as string;
+            Console.WriteLine((str3 != null) ? str3.ToString() : value.ToString());
+
+            //Approach 4 (null coalescing)
+            var str4 = value as string;
+            Console.WriteLine((str4 ?? value).ToString());
+
+            //Approach 5**
+            //var str5 = value is string;
+            if (value is string str5)
+                Console.WriteLine(str5.ToString());
+            else
+                Console.WriteLine(value.ToString());
+
+            //Approach 6  (null conditional)
+            var str6 = value as string;
+            Console.WriteLine(str6?.ToString());
+        }
         private static void PlayWithArrays()
         {
             //Console.WriteLine("How many names?");
@@ -239,6 +291,7 @@ namespace Section1
                 Console.WriteLine(message);
                 var input = Console.ReadLine();
 
+                // int.TryParse();
                 if (Int32.TryParse(input, out var result))
                 {
                     if (result >= minValue)
