d778e1a43ac8efcd2a76fcdbb5202d6240810167
Jeremy Lynch <Jeremy.Lynch@my.tccd.edu>
2018-11-09 21:01:09 -0600

    Lab 3

---
+++ Labs/ContactManager.UI/ContactManager.UI/MainForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContactManager.UI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OnHelpAbout( object sender, EventArgs e )
+        {
+            MessageBox.Show(this, "Jeremy Lynch\nITSE-140-20630\nContact Manager", "About", MessageBoxButtons.OK);
+        }
+
+        private void OnFileExit( object sender, EventArgs e )
+        {
+            if (MessageBox.Show("Are you sure you want to exit?",
+                "Close", MessageBoxButtons.YesNo) == DialogResult.No)
+                return;
+
+            Close();
+        }
+    }
+}
