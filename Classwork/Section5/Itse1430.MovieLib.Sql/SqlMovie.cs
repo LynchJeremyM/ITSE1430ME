0d7a6d209e5c933200b0885a09ac1754f3ec3fef
LynchJeremyM <jeremy.lynch@my.tccd.edu>
2018-11-14 17:28:44 -0600

    s

---
+++ Classwork/Section5/Itse1430.MovieLib.Sql/SqlMovie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itse1430.MovieLib.Sql
+{
+    // Provides additional information for the SQL database
+    internal class SqlMovie : Movie
+    {
+        public int Id { get; set; }
+    }
+}
